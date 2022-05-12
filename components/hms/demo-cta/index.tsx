65a9ddc3559df924aa328355bdbbbd17fa3e1408
roverflow <roverflow5@gmail.com>
2022-05-12 23:13:19 +0530

    Changes

--- components/hms/demo-cta/index.tsx
+++ components/hms/demo-cta/index.tsx
@@ -27,23 +27,8 @@ const DemoButton = () => {
   return (
     <Dialog.Root>
       <Dialog.Overlay className={cn(styles['overlay'])} />
-      <Dialog.Trigger asChild>
-        <button ref={ctaRef} id="cta-btn" className={cn(styles['cta-btn'])}>
-          Try Demo
-        </button>
-      </Dialog.Trigger>
-      <div id="cta-tooltip" className={cn(styles['tooltip'])}>
-        <InfoIcon />
-        Click here to demo a live webinar powered by 100ms
-      </div>
-      <Dialog.Content className={cn(styles['content'], 'dialog-animation')}>
-        <Dialog.Close asChild className={cn(styles['close-btn'])}>
-          <button>
-            <CrossIcon />
-          </button>
-        </Dialog.Close>
-        <DemoModal />
-      </Dialog.Content>
+
+      <Dialog.Content className={cn(styles['content'], 'dialog-animation')}></Dialog.Content>
     </Dialog.Root>
   );
 };
--- lib/constants.ts
+++ lib/constants.ts
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://code-infinity.vercel.app/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'TechClubJit';
+export const BRAND_NAME = 'Code Infinity';
+export const SITE_NAME_MULTILINE = ['Code', 'Infinity'];
+export const SITE_NAME = 'Code Infinity';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = '01 January 2022';
-export const SHORT_DATE = 'Jan 1 - 9:00am PST';
-export const FULL_DATE = 'Jan 1st 9am Pacific Time (GMT-7)';
+export const DATE = '02 July 2022';
+export const SHORT_DATE = 'July 2 - 9:00am IST';
+export const FULL_DATE = 'July 2nd 9am IST';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -37,36 +37,28 @@ export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
   'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+export const REPO = 'https://github.com/roverflow/Code-Infinity';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
-    name: 'Live Stage',
+    name: 'Home',
+    route: '/'
+  },
+  {
+    name: 'About',
     route: '/stage/a'
-  },
-  {
-    name: 'Vercel Stage',
-    route: '/stage/c'
-  },
-  {
-    name: '100ms Stage',
-    route: '/stage/m'
   },
   {
     name: 'Schedule',
     route: '/schedule'
   },
   {
-    name: 'Speakers',
+    name: 'Team',
     route: '/speakers'
   },
   {
-    name: 'Expo',
-    route: '/expo'
-  },
-  {
-    name: 'Jobs',
-    route: '/jobs'
+    name: 'Register',
+    route: '/stage/b'
   }
 ];
 
